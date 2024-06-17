cfb4122f598911ffa715ab670c207e8463e0e05e
John <136768731+jpt1729@users.noreply.github.com>
2024-06-17 16:14:39 -0400

    updates!

--- src/components/pages/organize-events/tip-tap-editor/index.js
+++ src/components/pages/organize-events/tip-tap-editor/index.js
@@ -1,10 +1,6 @@
 "use client";
 
-import {
-  useEditor,
-  EditorContent,
-  BubbleMenu,
-} from "@tiptap/react";
+import { useEditor, EditorContent, BubbleMenu } from "@tiptap/react";
 import Typography from "@tiptap/extension-typography";
 import Document from "@tiptap/extension-document";
 import Heading from "@tiptap/extension-heading";
@@ -12,7 +8,18 @@ import Paragraph from "@tiptap/extension-paragraph";
 import Text from "@tiptap/extension-text";
 import TextAlign from "@tiptap/extension-text-align";
 import StarterKit from "@tiptap/starter-kit";
-
+import ListItem from "@tiptap/extension-list-item";
+import OrderedList from "@tiptap/extension-ordered-list";
+import BulletList from '@tiptap/extension-bullet-list'
+import {
+  LeftAlignIcon,
+  CenterAlignIcon,
+  RightAlignIcon,
+  BulletListIcon,
+  OrderedListIcon,
+  UndoIcon,
+  RedoIcon,
+} from "./icons";
 function TipTapEditor() {
   const editor = useEditor({
     extensions: [
@@ -22,6 +29,9 @@ function TipTapEditor() {
       Paragraph,
       Text,
       Heading,
+      BulletList,
+      OrderedList,
+      ListItem,
       TextAlign.configure({
         types: ["heading", "paragraph"],
       }),
@@ -30,10 +40,79 @@ function TipTapEditor() {
   });
 
   return (
-    <>
+    <div className="flex flex-col gap-1">
+      <menu className="py-1 bg-white text-black shadow flex gap-1 rounded px-2 fill-black">
+        <button
+          onClick={() => editor.chain().focus().undo().run()}
+          className="font-semibold"
+        >
+          <UndoIcon className="size-6 " />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().redo().run()}
+          className="font-semibold"
+        >
+          <RedoIcon className="size-6 " />
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 1 }).run()
+          }
+          className="font-semibold"
+        >
+          H1
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          className="font-semibold"
+        >
+          H2
+        </button>
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 3 }).run()
+          }
+          className="font-semibold"
+        >
+          H3
+        </button>
+        <button onClick={() => editor.chain().focus().toggleBulletList().run()}>
+          <BulletListIcon className="size-6 " />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+        >
+          <OrderedListIcon className="size-6 " />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setTextAlign("left").run()}
+        >
+          <LeftAlignIcon className="size-6 " />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setTextAlign("center").run()}
+        >
+          <CenterAlignIcon className="size-6 " />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setTextAlign("right").run()}
+        >
+          <RightAlignIcon className="size-6 " />
+        </button>
+        <button
+          onClick={() => {
+            console.log(editor.getHTML());
+          }}
+        >
+          save
+        </button>
+      </menu>
+      <EditorContent editor={editor} className="bg-white p-2 rounded shadow" />
       {editor && (
         <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
-          <div className="px-2 py-1 flex gap-2 rounded bg-white shadow">
+          <div className="px-2 py-1 flex gap-2 rounded bg-white shadow ">
             <button
               onClick={() => editor.chain().focus().toggleBold().run()}
               className={`${
@@ -61,51 +140,7 @@ function TipTapEditor() {
           </div>
         </BubbleMenu>
       )}
-      <menu>
-        <button
-          onClick={() =>
-            editor.chain().focus().toggleHeading({ level: 1 }).run()
-          }
-        >
-          H1
-        </button>
-        <button
-          onClick={() =>
-            editor.chain().focus().toggleHeading({ level: 2 }).run()
-          }
-        >
-          H2
-        </button>
-        <button
-          onClick={() =>
-            editor.chain().focus().toggleHeading({ level: 3 }).run()
-          }
-        >
-          H3
-        </button>
-        <button
-          onClick={() => editor.chain().focus().setTextAlign("left").run()}
-        >
-          L
-        </button>
-        <button
-          onClick={() => editor.chain().focus().setTextAlign("center").run()}
-        >
-          C
-        </button>
-        <button
-          onClick={() => editor.chain().focus().setTextAlign("right").run()}
-        >
-          R
-        </button>
-        <button onClick={() => {
-          console.log(editor.getHTML())
-        }}>
-          save 
-        </button>
-      </menu>
-      <EditorContent editor={editor} />
-    </>
+    </div>
   );
 }
 
